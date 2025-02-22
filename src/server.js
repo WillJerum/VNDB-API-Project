1101ef79fa69440c6f5f251ef3701fd02fb47496
WillJerum <wrj2313@g.rit.edu>
2025-02-22 12:41:39 -0500

    Created necessary files and imported data

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const url = require('url'); // Import URL module
+
+const htmlHandler = require('./htmlResponses.js');
+const apiHandler = require('./apiResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
